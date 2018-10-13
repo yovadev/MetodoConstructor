fcb712fdc8dd2101e41fa80a9c00340a4ef9c0e8
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:47:25 -0600

    Initial commit

---
+++ src/metodoconstructor/MetodoConstructor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodoconstructor;
+
+/**
+ *
+ * @author Yova
+ */
+public class MetodoConstructor {
+
+    public MetodoConstructor(){
+        System.out.println("hola");
+    }
+    
+    public static void main(String[] args) {
+        MetodoConstructor prueba = new MetodoConstructor();
+        
+    }
+    
+}
